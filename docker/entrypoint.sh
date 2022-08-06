82ece4b2f5b30790f2ce18d23cd1fec41216b114
Higor Chaves <hhigorsilvachaves@gmail.com>
2022-08-05 21:15:05 -0300

    Add rspec as default test framework

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,8 +2,8 @@
 
 rm -f /projeto/tmp/pids/server.pid
 bundle install
-rails db:drop
-rails db:create
+# rails db:drop
+# rails db:create
 rails db:migrate
 # rails db:seed
 rails server --binding 0.0.0.0 --port $APPLICATION_PORT
