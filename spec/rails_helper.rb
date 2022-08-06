24e84efcaccc091a2969f9c00b7012ad1f0aa270
Higor Chaves <hhigorsilvachaves@gmail.com>
2022-08-05 22:46:54 -0300

    Add factory bot gem configs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,6 +2,7 @@
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+require './spec/support/factory_bot'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 abort('The Rails environment is running in production mode!') if Rails.env.production?
