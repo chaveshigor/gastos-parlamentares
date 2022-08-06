9cead854df18670349569937d945369d53377d15
Higor Chaves <hhigorsilvachaves@gmail.com>
2022-08-06 19:57:15 -0300

    Initialize Politician controller

---
+++ spec/requests/politicians_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe "Politicians", type: :request do
+  describe "GET /index" do
+    pending "add some examples (or delete) #{__FILE__}"
+  end
+end
