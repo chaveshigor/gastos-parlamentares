6a333559362f5878a80ec10e3fc0744aec596029
Higor Chaves <hhigorsilvachaves@gmail.com>
2022-08-06 19:36:54 -0300

    Rename upload controller spec

--- spec/requests/csv_uploads_controller_spec.rb
+++ spec/requests/csv_uploads_controller_spec.rb
@@ -2,8 +2,8 @@
 
 require 'rails_helper'
 
-RSpec.describe "Uploads", type: :request do
-  describe "GET /index" do
+RSpec.describe CsvUploadsController, type: :request do
+  describe 'GET #index' do
     pending "add some examples (or delete) #{__FILE__}"
   end
 end
