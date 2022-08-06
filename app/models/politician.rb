0c50fd418141d8dc3596fe83f94f811703971db9
Higor Chaves <hhigorsilvachaves@gmail.com>
2022-08-06 16:26:56 -0300

    Fix picture method

--- app/models/politician.rb
+++ app/models/politician.rb
@@ -8,7 +8,7 @@ class Politician < ApplicationRecord
   validates_presence_of :name, :uf
   validates_uniqueness_of :cpf, :registration_id, :wallet
 
-  def self.picture
+  def picture
     "https://www.camara.leg.br/internet/deputado/bandep/#{registration_id}.jpg"
   end
 end
