86894391f2b70fa64d25733820dff39c7159bece
Higor Chaves <hhigorsilvachaves@gmail.com>
2022-08-06 19:49:04 -0300

    Add CsvUploadsController tests

--- spec/requests/csv_uploads_controller_spec.rb
+++ spec/requests/csv_uploads_controller_spec.rb
@@ -2,8 +2,36 @@
 
 require 'rails_helper'
 
-RSpec.describe CsvUploadsController, type: :request do
-  describe 'GET #index' do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'CsvUploadsController', type: :request do
+  describe 'POST #uploader' do
+    let(:upload_request) { post uploader_csv_uploads_path }
+
+    context 'when file is valid' do
+      before do
+        runner = double('runner')
+        allow(CsvUploads::CsvHandlerService).to receive(:new).and_return(runner)
+        allow(runner).to receive(:run).and_return(true)
+      end
+
+      it 'render success message' do
+        upload_request
+
+        expect(flash[:notice]).to eq(I18n.t('csv_uploads.upload_success_message'))
+      end
+    end
+
+    context 'when file is invalid' do
+      before do
+        runner = double('runner')
+        allow(CsvUploads::CsvHandlerService).to receive(:new).and_return(runner)
+        allow(runner).to receive(:run).and_return(false)
+      end
+
+      it 'render success message' do
+        upload_request
+
+        expect(flash[:alert]).to eq(I18n.t('csv_uploads.upload_error_message'))
+      end
+    end
   end
 end
