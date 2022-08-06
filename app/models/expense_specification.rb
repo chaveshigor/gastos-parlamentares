0ba409ea60e66fda75671bcf75143f2975e3eb43
Higor Chaves <hhigorsilvachaves@gmail.com>
2022-08-06 11:00:05 -0300

    Add association as optional

--- app/models/expense_specification.rb
+++ app/models/expense_specification.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ExpenseSpecification < ApplicationRecord
-  belongs_to :expense_type
+  belongs_to :expense_type, optional: true
 
   validates_presence_of :description
   validates_uniqueness_of :code
--- spec/models/expense_specification_spec.rb
+++ spec/models/expense_specification_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe ExpenseSpecification, type: :model do
   describe 'associations' do
-    it { should belong_to(:expense_type) }
+    it { should belong_to(:expense_type).optional }
   end
 
   describe 'validations' do
