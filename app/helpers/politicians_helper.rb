2c33ce80abf0afebed37ce13650d244eb7084937
Higor Chaves <hhigorsilvachaves@gmail.com>
2022-08-06 22:24:23 -0300

    Add method to format currency

--- app/helpers/politicians_helper.rb
+++ app/helpers/politicians_helper.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 module PoliticiansHelper
+  include ActionView::Helpers::NumberHelper
+
+  def format_currency(amount)
+    number_to_currency(amount, unit: 'R$', separator: '.')
+  end
 end
