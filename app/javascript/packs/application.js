747bdc713f9f3e1f190cead63790e00ebad56a86
Higor Chaves <hhigorsilvachaves@gmail.com>
2022-08-07 08:59:54 -0300

    Add bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 import "chartkick/chart.js"
-
+import '../js/bootstrap_js_files.js'
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
