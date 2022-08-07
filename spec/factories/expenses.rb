49159c5a7ac14ce87c7194817ee2e99a39566edd
Higor Chaves <hhigorsilvachaves@gmail.com>
2022-08-06 23:13:54 -0300

    Refactor expenses factory

--- spec/factories/expenses.rb
+++ spec/factories/expenses.rb
@@ -3,16 +3,18 @@
 FactoryBot.define do
   factory :expense do
     document_number { Random.rand(10**5).to_s }
-    document_type { Random.rand(0...3).to_s }
+    document_type { Random.rand(0...5) }
     generation_date { Time.zone.today }
-    document_value { Random.rand(1...10**4) }
+    document_value { Random.rand(1...10**4).to_d }
     document_url { 'boleto_url' }
-    gloss_value { Random.rand(0...10**4) }
-    net_value { Random.rand(1...10**4) }
+    gloss_value { Random.rand(0...10**4).to_d }
+    net_value { Random.rand(1...10**4).to_d }
     month { Random.rand(1...13) }
     year { Random.rand(2018...2023) }
     installment_number { 1 }
     lot_number { Random.rand(10**5).to_s }
+    expense_type { build(:expense_type) }
+    provider { build(:provider) }
 
     trait :with_passanger do
       after(:build) do |expense|
