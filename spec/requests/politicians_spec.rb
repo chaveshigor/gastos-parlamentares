74e90f6c53e9317428f0283bf330afc7490643de
Higor Chaves <hhigorsilvachaves@gmail.com>
2022-08-06 23:14:22 -0300

    Add political controller tests

--- spec/requests/politicians_spec.rb
+++ spec/requests/politicians_spec.rb
@@ -2,8 +2,23 @@
 
 require 'rails_helper'
 
-RSpec.describe "Politicians", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Politicians', type: :request do
+  let!(:politicians) { create_list(:politician, 3, :with_expenses) }
+
+  describe 'GET #index' do
+    it 'return all politicians' do
+      get politicians_path
+
+      expect(assigns(:politicians)).to match_array(politicians)
+    end
+  end
+
+  describe 'GET #show' do
+    it 'return politician' do
+      get politician_path(politicians.first)
+
+      expect(assigns(:politician)).to eq(politicians.first)
+      expect(assigns(:expenses)).to match_array(politicians.first.expenses)
+    end
   end
 end
