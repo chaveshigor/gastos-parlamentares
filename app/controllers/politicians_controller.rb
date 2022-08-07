624fe94c43964bdaa04a5d4e6d20e9a579e8d32c
Higor Chaves <hhigorsilvachaves@gmail.com>
2022-08-06 22:24:04 -0300

    Add actions to politician controller

--- app/controllers/politicians_controller.rb
+++ app/controllers/politicians_controller.rb
@@ -1,4 +1,18 @@
 # frozen_string_literal: true
 
 class PoliticiansController < ApplicationController
+  def index
+    @politicians = Politician.all.order(name: :asc)
+  end
+
+  def show
+    @politician = Politician.find_by(id: politician_params[:id])
+    @expenses = @politician.expenses.includes(:provider).order(generation_date: :desc)
+  end
+
+  private
+
+  def politician_params
+    params.permit(:id)
+  end
 end
