07c6d54d345cf73ffe7f3ea145e4113c6918505e
Higor Chaves <hhigorsilvachaves@gmail.com>
2022-08-07 14:40:25 -0300

    Add loading to csv upload

---
+++ app/javascript/packs/loadNewData.js
@@ -0,0 +1,17 @@
+const btt = document.getElementById('sendDataBtt');
+const loadingDiv = document.getElementById('loading-div');
+const uploadDataDiv = document.getElementById('uploadDataDiv');
+const fileField = document.getElementById('fileField');
+
+btt.addEventListener('click', () => {
+  if(fileField.value != '' && fileField.value != null ) {
+    loadingDiv.style.display = 'flex';
+    loadingDiv.style.flexDirection = 'column';
+    loadingDiv.style.justifyContent = 'center';
+    loadingDiv.style.alignItems = 'center';
+    uploadDataDiv.style.display = 'none';
+  };
+
+  console.log(fileField.value, '<<<<<<');
+  //console.log(uploadFileForm.checkValidity())
+});
