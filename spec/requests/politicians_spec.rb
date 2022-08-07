cbb3ad71bee3cc6598b3b9e3868cc8e6e63b857c
Higor Chaves <hhigorsilvachaves@gmail.com>
2022-08-07 15:06:50 -0300

    Add delete_all action tests

--- spec/requests/politicians_spec.rb
+++ spec/requests/politicians_spec.rb
@@ -3,10 +3,10 @@
 require 'rails_helper'
 
 RSpec.describe 'Politicians', type: :request do
-  let!(:politicians) { create_list(:politician, 3, :with_expenses) }
-
   describe 'GET #index' do
     context 'when name search is not used' do
+      let!(:politicians) { create_list(:politician, 3, :with_expenses) }
+
       it 'return all politicians' do
         get politicians_path
 
@@ -15,20 +15,64 @@ RSpec.describe 'Politicians', type: :request do
     end
 
     context 'when name search is used' do
+      let!(:politicians) { create_list(:politician, 3, :with_expenses) }
+
       it 'return politicians matches' do
         get politicians_path, params: { query: { name_cont: politicians.last.name } }
 
         expect(assigns(:politicians)).to eq([politicians.last])
       end
     end
+
+    context 'when there is no politicians' do
+      it 'return flash message to request file upload' do
+        get politicians_path
+
+        expect(flash[:notice]).to eq(I18n.t('csv_uploads.upload_needed'))
+      end
+    end
   end
 
   describe 'GET #show' do
+    let!(:politicians) { create_list(:politician, 3, :with_expenses) }
+
     it 'return politician' do
       get politician_path(politicians.first)
 
       expect(assigns(:politician)).to eq(politicians.first)
       expect(assigns(:expenses)).to match_array(politicians.first.expenses)
+    end
+  end
+
+  describe 'DELETE #delete_all' do
+    let!(:politicians) { create_list(:politician, 3, :with_expenses) }
+
+    context 'when delete_all method return true' do
+      before do
+        runner = double('runner')
+        allow(Politicians::DeleteAllService).to receive(:new).and_return(runner)
+        allow(runner).to receive(:run).and_return(true)
+      end
+
+      it 'call delete_all service' do
+        delete destroy_all_politicians_path
+
+        expect(flash[:notice]).to eq(I18n.t('csv_uploads.destroy_success_message'))
+      end
+    end
+
+    context 'when delete_all method return false' do
+      before do
+        runner = double('runner')
+        allow(Politicians::DeleteAllService).to receive(:new).and_return(runner)
+        allow(runner).to receive(:run).and_return(false)
+      end
+
+      it 'call delete_all service' do
+        delete destroy_all_politicians_path
+
+        expect(flash[:alert]).to eq(I18n.t('csv_uploads.destroy_error_message'))
+      end
     end
   end
 end
