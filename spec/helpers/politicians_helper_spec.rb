9fab8ca775e47e184da9a5b236ddb7d21d53b45d
Higor Chaves <hhigorsilvachaves@gmail.com>
2022-08-06 22:33:00 -0300

    Add politician helper tests

--- spec/helpers/politicians_helper_spec.rb
+++ spec/helpers/politicians_helper_spec.rb
@@ -3,5 +3,10 @@
 require 'rails_helper'
 
 RSpec.describe PoliticiansHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#format_currency' do
+    it 'format to brasilian currency' do
+      amount = 12_365.99
+      expect(format_currency(amount)).to eq('R$12,365.99')
+    end
+  end
 end
