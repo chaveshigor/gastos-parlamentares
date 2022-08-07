4a315013df84927c486297fd9ae34a80414e65df
Higor Chaves <hhigorsilvachaves@gmail.com>
2022-08-07 14:42:12 -0300

    Add destroy all action to controller

--- app/controllers/politicians_controller.rb
+++ app/controllers/politicians_controller.rb
@@ -6,11 +6,25 @@ class PoliticiansController < ApplicationController
   def index
     @politicians = params[:query].blank? ? Politician.all : @query.result
     @politicians = @politicians.order(name: :asc)
+
+    if @politicians.blank?
+      flash[:notice] = t('csv_uploads.upload_needed')
+      redirect_to csv_uploads_path
+    end
   end
 
   def show
     @politician = Politician.find_by(id: politician_params[:id])
     @expenses = @politician.expenses.includes(:provider).order(generation_date: :desc)
+  end
+
+  def destroy_all
+    result = Politicians::DeleteAllService.new.run
+
+    flash[:alert] = t('csv_uploads.destroy_error_message') unless result
+    flash[:notice] = t('csv_uploads.destroy_success_message') if result
+
+    redirect_to csv_uploads_path
   end
 
   private
